0b7d451814bb3ad573dd8e31260fa8357ad504bb
deiseantunes <deiseantunes.ds@gmail.com>
2021-11-26 21:44:31 +0000

    Exercicio1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace dotnetcore
 {
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
-
-
-        // Exercício 2:
+        // ExercÃ­cio 2:
         // - Fazer um programa que cadastre pessoas, contendo nome e idade;
         // - Imprimir a media de idade das pessoas
         // - Imprimir o nome e idade da pessoa mais velha
         // - Imprimir o nome e idade da pessoa mais nova
         // - Imprimir a quantidade de pessoas com maior idade
-        // Requisítos:
+        // RequisÃ­tos:
         // - Utilizar List (Avg, Max, Min, Where)
         // - Utilizar Classe Pessoa
         static void Main(string[] args)
         {
             Console.WriteLine("Iniciando Programa");
 
-            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
-            var xNotasBronza = new int[] { 8, 9, 10, 8 };
+            var xNotasZuqui = new List<int> { 7, 6, 5, 3 };
+            var xNotasBronza = new List<int> { 8, 9, 10, 8 };
+            var xMediaZuqui = CalcularMedia(xNotasZuqui);
+            Console.WriteLine("MÃ©dia do Zuqui: " + xMediaZuqui);
+            ImprimirResultado(xMediaZuqui);
+
+
+            var xMediaBronza = CalcularMedia(xNotasBronza);
+            //Console.WriteLine("MÃ©dia do Bronza: " + CalcularMedia(xNotasBronza));
+            Console.WriteLine("MÃ©dia do Bronza: " + xMediaBronza);
+            ImprimirResultado(xMediaBronza);
+
+
+            //MÃ©todo Caclular mÃ©dia
+            float CalcularMedia(List<int> pNotas)
+            {
+                var xMedia = 0;
+                var xSomaNotas = 0;
+                var xQuantidadeItens = 0;
+
+               foreach (var xNota in pNotas)
+               {
+                   xSomaNotas = xSomaNotas + xNota;
+                   xQuantidadeItens = xQuantidadeItens + 1;
+                   xMedia = xSomaNotas / xQuantidadeItens;
+               }
+
+               return xMedia;
+
+            }
+
+            static void ImprimirResultado(float pMedia)
+            {
+                if (pMedia >= 7)
+                {
+                    Console.WriteLine ("Passou no ano letivo");
+                }
+                else
+                {
+                    Console.WriteLine ("NÃ£o passou no ano letivo");
+                }
+            }
         }
     }
 }
