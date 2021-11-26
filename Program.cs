b276639e52433da3c4fe7db1a17dd2248a0db86d
deiseantunes <deiseantunes.ds@gmail.com>
2021-11-26 21:46:08 +0000

    Exercicio 2

--- Program.cs
+++ Program.cs
@@ -1,71 +1,98 @@
 ﻿using System;
+
+using System.Linq;
+
 using System.Collections.Generic;
 
 
+
+
 namespace dotnetcore
+
 {
+
     public class Pessoa
+
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
-    }
-    class Program
-    {
-        // ExercÃ­cio 2:
-        // - Fazer um programa que cadastre pessoas, contendo nome e idade;
-        // - Imprimir a media de idade das pessoas
-        // - Imprimir o nome e idade da pessoa mais velha
-        // - Imprimir o nome e idade da pessoa mais nova
-        // - Imprimir a quantidade de pessoas com maior idade
-        // RequisÃ­tos:
-        // - Utilizar List (Avg, Max, Min, Where)
-        // - Utilizar Classe Pessoa
-        static void Main(string[] args)
+
+        public Pessoa (string pNome, int pIdade)
+
         {
-            Console.WriteLine("Iniciando Programa");
 
-            var xNotasZuqui = new List<int> { 7, 6, 5, 3 };
-            var xNotasBronza = new List<int> { 8, 9, 10, 8 };
-            var xMediaZuqui = CalcularMedia(xNotasZuqui);
-            Console.WriteLine("MÃ©dia do Zuqui: " + xMediaZuqui);
-            ImprimirResultado(xMediaZuqui);
+            Nome = pNome;
 
+            Idade = pIdade;
 
-            var xMediaBronza = CalcularMedia(xNotasBronza);
-            //Console.WriteLine("MÃ©dia do Bronza: " + CalcularMedia(xNotasBronza));
-            Console.WriteLine("MÃ©dia do Bronza: " + xMediaBronza);
-            ImprimirResultado(xMediaBronza);
-
-
-            //MÃ©todo Caclular mÃ©dia
-            float CalcularMedia(List<int> pNotas)
-            {
-                var xMedia = 0;
-                var xSomaNotas = 0;
-                var xQuantidadeItens = 0;
-
-               foreach (var xNota in pNotas)
-               {
-                   xSomaNotas = xSomaNotas + xNota;
-                   xQuantidadeItens = xQuantidadeItens + 1;
-                   xMedia = xSomaNotas / xQuantidadeItens;
-               }
-
-               return xMedia;
-
-            }
-
-            static void ImprimirResultado(float pMedia)
-            {
-                if (pMedia >= 7)
-                {
-                    Console.WriteLine ("Passou no ano letivo");
-                }
-                else
-                {
-                    Console.WriteLine ("NÃ£o passou no ano letivo");
-                }
-            }
         }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
     }
+
+    class Program
+
+    {
+
+        // Exercício 2:
+
+        // - Fazer um programa que cadastre pessoas, contendo nome e idade;
+
+        // - Imprimir a media de idade das pessoas
+
+        // - Imprimir o nome e idade da pessoa mais velha
+
+        // - Imprimir o nome e idade da pessoa mais nova
+
+        // - Imprimir a quantidade de pessoas com maior idade
+
+        // Requisítos:
+
+        // - Utilizar List (Avg, Max, Min, Where)
+
+        // - Utilizar Classe Pessoa
+
+        static void Main(string[] args)
+
+        {
+
+            var xListaPessoas = new List<Pessoa>{
+
+                new Pessoa ("Deise", 20),
+
+                new Pessoa("Gabi", 19)
+
+            };
+
+
+
+            var xMedia = xListaPessoas.Average(p => p.Idade);
+
+            var xPessoaMaisVelha = xListaPessoas.OrderByDescending(p => p.Idade).FirstOrDefault();
+
+            var xPessoaMaisNova = xListaPessoas.OrderBy(p => p.Idade).FirstOrDefault();
+
+            var xQuantidadePessoasMaioresIdade = xListaPessoas.Where(p => p.Idade >= 18).Count();
+
+           
+
+            Console.WriteLine ("A media de idade das pessoas é: " + xMedia);
+
+
+
+            Console.WriteLine ("O nome da pessoa mais velha é: " + xPessoaMaisVelha.Nome + " e a idade é: " + xPessoaMaisVelha.Idade);
+
+            Console.WriteLine ("O nome da pessoa mais nova é: " + xPessoaMaisNova.Nome + " e a idade é: " + xPessoaMaisNova.Idade);
+
+            Console.WriteLine ("A media de idade das pessoas é: " + xQuantidadePessoasMaioresIdade);
+
+         
+
+
+
+        }
+
+    }
+
 }
